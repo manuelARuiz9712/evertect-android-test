22b06c3f0ac90ee7d28c233c2fbb285690265635
=Manuel angulo <manuelanguloruiz@gmail.com>
2021-10-18 21:36:05 -0500

    Adding views to class

---
+++ app/src/main/java/com/example/evertec_app/views/card/AddCard.java
@@ -0,0 +1,16 @@
+package com.example.evertec_app.views.card;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.evertec_app.R;
+
+public class AddCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+    }
+}
---
+++ app/src/main/java/com/example/evertec_app/views/payment/AddPayment.java
@@ -0,0 +1,16 @@
+package com.example.evertec_app.views.payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.evertec_app.R;
+
+public class AddPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_payment);
+    }
+}
